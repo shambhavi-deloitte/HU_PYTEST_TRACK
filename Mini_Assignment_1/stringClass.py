8e29333e372f51854b3ee7cc90c4c5ba4d48ca94
shampriya <shampriya@deloitte.com>
2022-05-17 23:35:47 +0530

    String_Class

---
+++ Mini_Assignment_1/stringClass.py
@@ -0,0 +1,15 @@
+class StringClass:
+    # Constructor
+    def __init__(self, s):
+        self.s = s
+
+    def lengthOfString(self):
+        return len(self.s)
+
+    def ConvertStringToList(self):
+        return list(map(str, self.s))
+
+
+obj = StringClass('12314532')
+print(obj.lengthOfString())
+print(obj.ConvertStringToList())
