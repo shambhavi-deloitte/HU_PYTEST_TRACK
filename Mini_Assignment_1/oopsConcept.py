e1659f42eba1b4ead7d41443ffa00273d7429a9e
shampriya <shampriya@deloitte.com>
2022-05-17 23:54:48 +0530

    SearchCommonElement_Class

--- Mini_Assignment_1/oopsConcept.py
+++ Mini_Assignment_1/oopsConcept.py
@@ -31,7 +31,25 @@ class PairsPossible(StringClass):
     def GetString(self):
         return self.s
 
+class SearchCommonElements:
+    def __init__(self, s, s1, s2):
+        self.s = s
+        self.s1 = s1
+        self.s2 = s2
 
-# obj = StringClass('12314532')
-# print(obj.lengthOfString())
-# print(obj.ConvertStringToList())
+    def SearchElements(self):
+        res = [];
+        dict1 = {};
+        dict2 = {}
+        for c in list(map(str, self.s)):
+            if c in list(map(str, self.s1)):
+                if dict1.get(c) is None:
+                    dict1[c] = 1
+                else:
+                    dict1[c] += 1
+            if c in self.s2:
+                if dict2.get(c) is None:
+                    dict2[c] = 1
+                else:
+                    dict2[c] += 1
+                    res = [len(dict1), len(dict2)]
