82c7e5d61fe87139af4537b07e190435b1680b0b
shampriya <shampriya@deloitte.com>
2022-05-17 23:48:49 +0530

    String_Class

--- Mini_Assignment_1/oopsConcept.py
+++ Mini_Assignment_1/oopsConcept.py
@@ -12,24 +12,7 @@ class StringClass:
     def ConvertStringToList(self):
         return list(map(str, self.s))
 
-class PairsPossible(StringClass):
-    def __init__(self, arr):
-        self.arr = arr
-        StringClass.__init__(self, arr)
 
-    def StoreAllPossiblePairs(self):
-        self.arr = list(combinations(self.s, 2))
-
-    def PrintAllPossiblePairs(self):
-        for i in self.arr:
-            print(list(i), end=' ')
-            print()
-
-    def GetAllPossiblePairs(self):
-        return self.arr
-
-    def GetString(self):
-        return self.s
 
 
 # obj = StringClass('12314532')
